6221263ad35565015d151b90b851cdf661e6a7f5
lishimin898 <58264714+lishimin898@users.noreply.github.com>
2020-10-01 14:03:53 +0800

    Update mloop_visual.py

--- mloop_visual.py
+++ mloop_visual.py
@@ -1,4 +1,5 @@
 import mloop.visualizations as mlv
+import matplotlib.pyplot as plt
 
 def neuralnet_visualization_from_archieve(datetime):
     file_path = './M-LOOP_archives/'
@@ -9,3 +10,19 @@ def neuralnet_visualization_from_archieve(datetime):
     mlv.show_all_default_visualizations_from_archive(controller_filename=controller_filename,
                                                      learner_filename=learner_filename,
                                                      controller_type='neural_net')
+    plt.show(
+    
+def visualization_from_controller_and_learner_archive(datetime)
+    rootpath='./M-LOOP_archives'
+    filetype='txt'
+    #date='2020-09-16_16-23'
+    controller_archive_filename=rootpath+'/controller_archive_'+date+'.'+filetype
+    learner_archive_filename=rootpath+'/learner_archive_'+date+'.'+filetype
+    mlv.create_controller_visualizations(controller_archive_filename,
+                                    file_type=filetype,
+                                    max_parameters_per_plot=2)
+    mlv.create_neural_net_learner_visualizations(learner_archive_filename, 
+                                                file_type=filetype,
+                                                plot_cross_sections=True)
+    plt.show()
+    
